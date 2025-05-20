dcbb49e9b48770d380b9ae6077287ed9417163ff
noboKumar <nobokumar227@gmail.com>
2025-05-20 14:09:53 +0600

    setup react-router

---
+++ src/routes/Router.jsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
