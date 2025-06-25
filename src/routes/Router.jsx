54b8ac8e6d09b88799be36132e1762a791d7e4e1
noboKumar <nobokumar227@gmail.com>
2025-06-26 04:21:47 +0600

    add error page in dashboard

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -115,6 +115,10 @@ export const router = createBrowserRouter([
           </PrivateRouter>
         ),
       },
+      {
+        path: "*",
+        Component: ErrorPage,
+      },
     ],
   },
 ]);
