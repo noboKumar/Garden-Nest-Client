d687ca2f87185086e171c939fc00eb4a1f5210c8
noboKumar <nobokumar227@gmail.com>
2025-06-27 00:47:55 +0600

    fix top trending card and add see more button

--- src/Components/TopTrendingTipsCard.jsx
+++ src/Components/TopTrendingTipsCard.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 import { FaRegHeart } from "react-icons/fa";
+import { Link } from "react-router";
 
 const TopTrendingTipsCard = ({ trending }) => {
-  const { imageURL, title, description, likedBy } = trending;
+  const { imageURL, title, description, likedBy, _id } = trending;
   return (
-    <div className="space-y-2 border-2 border-secondary bg-base-200 rounded-2xl px-10 py-6 shadow-sm cursor-pointer">
+    <div className="flex flex-col border-2 border-secondary bg-base-200 rounded-2xl px-5 py-4 shadow-sm ">
       {imageURL && (
         <img
-          className=" h-40 object-cover rounded-2xl mx-auto"
+          className="h-40 object-cover rounded-2xl mx-auto"
           src={imageURL}
           alt="trending tips photo"
         />
       )}
-      <h1 className="text-2xl text-center">{title}</h1>
-      <div className="flex items-center gap-2">
+      <h1 className="text-2xl text-center mt-2">{title}</h1>
+      <div className="flex items-center gap-2 mt-2">
         <FaRegHeart />
         <div>
           <span className="font-bold">Total Liked: </span>
           {likedBy?.length}
         </div>
       </div>
-      <p>{description}</p>
+      <p className="line-clamp-3 grow">{description}</p>
+      <Link to={`/BrowseTips/${_id}`} className="btn btn-outline btn-primary mt-auto">
+        See More
+      </Link>
     </div>
   );
 };
